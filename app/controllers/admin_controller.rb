3760e99f51117aeb1c6b5972e79cdb4fe9a9da26
Rémy Hannequin <remy.hannequin@gmail.com>
2017-01-24 18:25:20 +0100

    Add some good practices and enhancements

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,5 @@
 class AdminController < ApplicationController
   before_action :authenticate_user!
-  load_and_authorize_resource
+  authorize_resource
   layout 'admin'
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Add some fields to Devise account models
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # Redirect if trying to access an unauthorized action
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
     redirect_back(fallback_location: root_url)
