cc37429243165477bc1b7ad5a91e26d5b8539b15
Rémy Hannequin <remy.hannequin@gmail.com>
2017-01-24 23:58:26 +0100

    [fix] Fix issues with translations and routes

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,5 @@
 class AdminController < ApplicationController
   before_action :authenticate_user!
-  authorize_resource
+  load_and_authorize_resource
   layout 'admin'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks' }
   root to: 'home#index'
 
-  authenticate :user do
-    namespace :admin do
-      resources :users, only: [:index, :show, :destroy]
-    end
+  namespace :admin do
+    resources :users, only: [:index, :show, :destroy]
   end
 end
