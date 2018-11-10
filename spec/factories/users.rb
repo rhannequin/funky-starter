8e6fa6fbd643da87b2d4c1ae813ad689ef9ab99e
Rémy Hannequin <remy.hannequin@gmail.com>
2018-11-10 17:16:50 +0100

    Update gems and Ruby versions

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,21 +5,21 @@ FactoryBot.define do
     name { Faker::Name.name }
     sequence(:email) { |n| Faker::Internet.email("user_#{n}") }
 
-    password "password"
+    password { "password" }
     password_confirmation { password }
 
     trait :with_facebook_account do
-      provider "facebook"
+      provider { "facebook" }
       sequence(:uid) { |n| "#{provider}-user-#{n}" }
     end
 
     trait :with_twitter_account do
-      provider "twitter"
+      provider { "twitter" }
       sequence(:uid) { |n| "#{provider}-user-#{n}" }
     end
 
     trait :already_signed_in do
-      last_sign_in_at Date.new
+      last_sign_in_at { Date.new }
       last_sign_in_ip { Faker::Internet.ip_v4_address }
     end
   end
